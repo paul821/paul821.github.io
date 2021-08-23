740e3049738ab5f851cab800c4efb2ed5bf3bd25
Paul Kim <41440621+paul821@users.noreply.github.com>
2021-08-23 23:04:35 +0900

    Update collapse.js

--- collapse.js
+++ collapse.js
@@ -1,19 +1,25 @@
 function collapse(self) {
-  if (self.value === ">"){
-    var cont = document.querySelectorAll('.con');
-    var i = 0;
-    while (i < cont.length){
-      cont[i].style.display='none';
-      i++;
-    }
-    self.value = "^";
-  } else {
+  //self is heading
+  if (self.opacity = "0.5"){
+    //collapsed already. need to uncollapse
     var cont = document.querySelectorAll('.con');
     var i = 0;
     while (i < cont.length){
       cont[i].style.display='block';
       i++;
     }
-    self.value = ">";
+    self.opacity="1";
+    $(self).attr('\f125','\f123');
+});
+    
+  } else {
+    var cont = document.querySelectorAll('.con');
+    var i = 0;
+    while (i < cont.length){
+      cont[i].style.display='none';
+      i++;
+    }
+    self.opacity="0.5";
+    $(self).attr('\f123','\f125');
   }
 }
