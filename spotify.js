7b21864264b96de2b71cbb2954327927b47741f2
Paul Kim <41440621+paul821@users.noreply.github.com>
2021-08-26 02:29:11 +0900

    Update spotify.js

--- spotify.js
+++ spotify.js
@@ -8,3 +8,11 @@ for (let i = 0; i < musicLists.length; i++) {
     rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)), ${bgImage}`;
   musicLists[i].style.opacity = 0.8;
 }
+
+
+let audio = new Audio("http://21273.live.streamtheworld.com/LOS40_DANCE.mp3");
+
+let volume = document.querySelector("#volume-control");
+volume.addEventListener("change", function(e) {
+audio.volume = e.currentTarget.value / 100;
+})
