26e3f369c966c5b43452b03a355763d0322fa6aa
Paul Kim <41440621+paul821@users.noreply.github.com>
2021-08-28 02:46:08 +0900

    Update spotify.js

--- spotify.js
+++ spotify.js
@@ -1,18 +1,47 @@
 
-const musicLists = document.getElementsByClassName('music__list-item');
+var dict={}
+dict['photo']='https://www.youtube.com/watch?v=ndtQ6ReXO-s';
+dict['music']='https://www.youtube.com/watch?v=gGdGFtwCNBE';
+dict['writing']='https://www.youtube.com/watch?v=eYiDIeSk3Go';
+dict['baseball']='https://www.youtube.com/watch?v=C2EnB6ldpVo';
+dict['wrestling']='https://www.youtube.com/watch?v=1uBPOu3si5w';
+dict['lang']='https://www.youtube.com/watch?v=bY3Fhm9oTyI';
+dict['movie']='https://www.youtube.com/watch?v=cTE7k6Toe3M';
+
+function Play(x)
+      {
+        var audiolink = dict[x];
+        var myAudio = document.createElement('audio');
+        document.body.appendChild(myAudio);
+        myAudio.src=audiolink;
+        myAudio.play();
+        
+        
+        //if(myAudio.paused) {
+        //    myAudio.play();
+        //}
+        //else {
+        //   myAudio.pause();
+        //}
+      }
 
 
-for (let i = 0; i < musicLists.length; i++) {
-  const bgImage = `url('https://source.unsplash.com/random/300x300?sig=${i}')`;
-  musicLists[i].style.backgroundImage = `linear-gradient(
-    rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)), ${bgImage}`;
-  musicLists[i].style.opacity = 0.8;
-}
-
-
-let audio = new Audio("http://21273.live.streamtheworld.com/LOS40_DANCE.mp3");
+let audio = document.getElementsByTagName('audio');
 
 let volume = document.querySelector("#volume-control");
 volume.addEventListener("change", function(e) {
 audio.volume = e.currentTarget.value / 100;
 })
+
+
+
+function playpause() {
+  myAudio = document.getElementsByTagName('audio');
+    if(myAudio.paused) {
+            myAudio.play();
+        }
+        else {
+           myAudio.pause();
+        }
+}
+
