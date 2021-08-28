0560b38c74fd7ac1096f712c3454c9e0bfe6d64a
Paul Kim <41440621+paul821@users.noreply.github.com>
2021-08-28 23:08:26 +0900

    Update spotify.js

--- spotify.js
+++ spotify.js
@@ -8,14 +8,24 @@ dict['wrestling']='spotifyaudio/wrestling.mp3';
 dict['lang']='spotifyaudio/lang.mp3';
 dict['movie']='spotifyaudio/movie.mp3';
 
+var vid = {}
+vid['photo']='https://www.youtube.com/watch?v=i1l9VeVDkhE';
+vid['music']='https://www.youtube.com/watch?v=gGdGFtwCNBE';
+vid['writing']='https://www.youtube.com/watch?v=eYiDIeSk3Go';
+vid['baseball']='https://www.youtube.com/watch?v=EmTJVTTCg6k';
+vid['wrestling']='https://www.youtube.com/watch?v=bh9197p2-rc';
+vid['lang']='https://www.youtube.com/watch?v=_KzHGbpxMOY';
+vid['movie']='https://www.youtube.com/watch?v=FqAjVAf5fNA';
+
 
 var track=document.getElementById("bgm"); 
 
 function Play(x)
 {
     var audiolink = dict[x];
+    var videolink = vid[x];
     
-    document.getElementById("albumimg").src=audiolink;
+    document.getElementById("albumimg").src=videolink;
     
     track.src=audiolink;
     track.autoplay=true;
