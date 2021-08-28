a8ec99ba7fc16fe7edd3a0b03b7e4fc1b3757928
Paul Kim <41440621+paul821@users.noreply.github.com>
2021-08-28 22:15:41 +0900

    Update spotify.js

--- spotify.js
+++ spotify.js
@@ -1,29 +1,47 @@
 
 var dict={}
-dict['photo']='https://www.youtube.com/embed/ndtQ6ReXO-s?enablejsapi=1&controls=0&autoplay=1&html5=1';
-dict['music']='https://www.youtube.com/embed/gGdGFtwCNBE?enablejsapi=1&controls=0&autoplay=1&html5=1';
-dict['writing']='https://www.youtube.com/embed/eYiDIeSk3Go?enablejsapi=1&controls=0&autoplay=1&html5=1';
-dict['baseball']='https://www.youtube.com/embed/C2EnB6ldpVo?enablejsapi=1&controls=0&autoplay=1&html5=1';
-dict['wrestling']='https://www.youtube.com/embed/1uBPOu3si5w?enablejsapi=1&controls=0&autoplay=1&html5=1';
-dict['lang']='https://www.youtube.com/embed/bY3Fhm9oTyI?enablejsapi=1&controls=0&autoplay=1&html5=1';
-dict['movie']='https://www.youtube.com/embed/cTE7k6Toe3M?enablejsapi=1&controls=0&autoplay=1&html5=1';
+dict['photo']='spotifyaudio/photo.mp3';
+dict['music']='spotifyaudio/music.mp3';
+dict['writing']='spotifyaudio/writing.mp3';
+dict['baseball']='spotifyaudio/baseball.mp3';
+dict['wrestling']='spotifyaudio/wrestling.mp3';
+dict['lang']='spotifyaudio/lang.mp3';
+dict['movie']='spotifyaudio/movie.mp3';
+
+
+var track=document.getElementById("bgm"); 
 
 function Play(x)
-      {
-        
-        var audiolink = dict[x];
-        
-        document.getElementById("albumimg").src=audiolink;
-        
-        
-        //if(myAudio.paused) {
-        //    myAudio.play();
-        //}
-        //else {
-        //   myAudio.pause();
-        //}
-      }
+{
+    var audiolink = dict[x];
+    
+    document.getElementById("albumimg").src=audiolink;
+    
+    track.src=audiolink;
+    track.autoplay=true;
+}
 
+
+var playButton = document.getElementById("fa-play");    
+
+playbutton.addEventListener('click', function() {
+  if (track.paused) {
+    track.play();
+  } else {
+    track.pause();
+  }
+});
+
+
+
+var repeatButton = document.getElementById("fa-repeat");
+repeatButton.addEventListener('click',function() {
+  if (track.loop=true){
+    track.loop=false;
+  } else {
+    track.loop=true;
+  }
+});
 
 //let audio = document.getElementsByTagName('audio');
 
@@ -32,37 +50,6 @@ function Play(x)
 //audio.volume = e.currentTarget.value / 100;
 //})
 
-
-// global variable for the player
-var player;
-
-// this function gets called when API is ready to use
-function onYouTubePlayerAPIReady() {
-    // create the global player from the specific iframe (#video)
-    player = new YT.Player('albumimg', {
-        events: {
-            // call this function when player is ready to use
-            'onReady': onPlayerReady
-        }
-    });
-}
-
-
-function onPlayerReady(event) {
-
-    // bind events
-    var playButton = document.getElementById("fa-play");
-    playButton.addEventListener("click", function() {
-          if(YT.PlayerState.PLAYING) {
-                player.pauseVideo();
-          } else {
-                player.playVideo();
-          }
-        
-    });
-
-
-}
 
 
 
