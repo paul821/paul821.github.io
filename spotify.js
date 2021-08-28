45fb0ba8c330d47a1c2825cfe645c9ca85d8ac46
Paul Kim <41440621+paul821@users.noreply.github.com>
2021-08-28 17:10:19 +0900

    Update spotify.js

--- spotify.js
+++ spotify.js
@@ -1,23 +1,20 @@
 
 var dict={}
-dict['photo']='https://www.youtube.com/watch?v=ndtQ6ReXO-s';
-dict['music']='https://www.youtube.com/watch?v=gGdGFtwCNBE';
-dict['writing']='https://www.youtube.com/watch?v=eYiDIeSk3Go';
-dict['baseball']='https://www.youtube.com/watch?v=C2EnB6ldpVo';
-dict['wrestling']='https://www.youtube.com/watch?v=1uBPOu3si5w';
-dict['lang']='https://www.youtube.com/watch?v=bY3Fhm9oTyI';
-dict['movie']='https://www.youtube.com/watch?v=cTE7k6Toe3M';
+dict['photo']='https://www.youtube.com/watch?v=ndtQ6ReXO-s?enablejsapi=1&controls=0&autoplay=1&html5=1';
+dict['music']='https://www.youtube.com/watch?v=gGdGFtwCNBE?enablejsapi=1&controls=0&autoplay=1&html5=1';
+dict['writing']='https://www.youtube.com/watch?v=eYiDIeSk3Go?enablejsapi=1&controls=0&autoplay=1&html5=1';
+dict['baseball']='https://www.youtube.com/watch?v=C2EnB6ldpVo?enablejsapi=1&controls=0&autoplay=1&html5=1';
+dict['wrestling']='https://www.youtube.com/watch?v=1uBPOu3si5w?enablejsapi=1&controls=0&autoplay=1&html5=1';
+dict['lang']='https://www.youtube.com/watch?v=bY3Fhm9oTyI?enablejsapi=1&controls=0&autoplay=1&html5=1';
+dict['movie']='https://www.youtube.com/watch?v=cTE7k6Toe3M?enablejsapi=1&controls=0&autoplay=1&html5=1';
 
 function Play(x)
       {
-        var audiolink = dict[x];
-        var myAudio = document.createElement('audio');
-        document.body.appendChild(myAudio);
-        myAudio.src=audiolink;
-        myAudio.play();
         
-        document.getElementById("albumimg").src="spotifyimages/"+encodeURIComponent(x)+".JPG";
-            
+        var audiolink = dict[x];
+        
+        document.getElementById("albumimg").src=audiolink;
+        
         
         //if(myAudio.paused) {
         //    myAudio.play();
@@ -36,14 +33,36 @@ function Play(x)
 //})
 
 
+// global variable for the player
+var player;
 
-function playpause() {
-  myAudio = document.getElementsByTagName('audio');
-    if(myAudio.paused) {
-            myAudio.play();
+// this function gets called when API is ready to use
+function onYouTubePlayerAPIReady() {
+    // create the global player from the specific iframe (#video)
+    player = new YT.Player('albumimg', {
+        events: {
+            // call this function when player is ready to use
+            'onReady': onPlayerReady
         }
-        else {
-           myAudio.pause();
-        }
+    });
 }
+
+
+function onPlayerReady(event) {
+
+    // bind events
+    var playButton = document.getElementById("fa-play");
+    playButton.addEventListener("click", function() {
+          if(YT.PlayerState.PLAYING) {
+                player.pauseVideo();
+          } else {
+                player.playVideo();
+          }
+        
+    });
+
+
+}
+
+
 
